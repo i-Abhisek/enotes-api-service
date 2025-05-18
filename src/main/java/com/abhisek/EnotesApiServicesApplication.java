9209d6395a77819c97e6dc99de85343db7368aa1
Abhisek <abhiseksahoo7735@gmail.com>
2025-05-18 13:13:51 +0530

    intial commit

---
+++ src/main/java/com/abhisek/EnotesApiServicesApplication.java
@@ -0,0 +1,13 @@
+package com.abhisek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesApiServicesApplication.class, args);
+	}
+
+}
