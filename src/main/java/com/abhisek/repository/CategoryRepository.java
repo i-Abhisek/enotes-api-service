251ab19be732173bb427e3dd8323f7da829988af
Abhisek <abhiseksahoo7735@gmail.com>
2025-05-23 00:09:41 +0530

    upload implementation

--- src/main/java/com/abhisek/repository/CategoryRepository.java
+++ src/main/java/com/abhisek/repository/CategoryRepository.java
@@ -4,14 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.abhisek.entity.Category;
 
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
 
-	List<Category> findByIsActiveTrueAndDeletedFalse();
+	List<Category> findByIsActiveTrueAndIsDeletedFalse();
 
 	Optional<Category> findByIdAndIsDeletedFalse(Integer id);
 
 	List<Category> findByIsDeletedFalse();
+
 
 }
--- src/main/java/com/abhisek/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/abhisek/service/impl/CategoryServiceImpl.java
@@ -1,16 +1,17 @@
 package com.abhisek.service.impl;
 
 import java.util.Date;
-
 import java.util.List;
 import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import com.abhisek.dto.CategoryDto;
-import com.abhisek.dto.CategoryResponse;
+import com.abhisek.dto.CategoryResponse;  // fixed import
+
 import com.abhisek.entity.Category;
 import com.abhisek.repository.CategoryRepository;
 import com.abhisek.service.CategoryService;
@@ -18,83 +19,75 @@ import com.abhisek.service.CategoryService;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    @Autowired
-    private CategoryRepository categoryRepo;
-    
-    @Autowired
-    private ModelMapper mapper;
-    
-    @Override
-    public Boolean saveCategory(CategoryDto categoryDto) {
-        try {
-        	
-        Category category	= mapper.map(categoryDto, Category.class);
-        	
-            // Category category = new Category();
-        		
-            category.setName(categoryDto.getName());
-            category.setDescription(categoryDto.getDescription());
-            category.setIsActive(categoryDto.getIsActive());
-            category.setIsDeleted(false);
-            category.setCreatedBy(1);
-            category.setCreateOn(new Date());
+	@Autowired
+	private CategoryRepository categoryRepo;
 
-            categoryRepo.save(category);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    @Override
-    public List<CategoryDto> getAllCategory() {
-    	
-    	List<Category>categories = categoryRepo.findByIsDeletedFalse();
-    	List<CategoryDto>categoryDtoList = categories.stream().map(cat->mapper.map(cat,CategoryDto.class)).toList();
-    	
-        return categoryDtoList;
-    }
+	@Autowired
+	private ModelMapper mapper;
 
 	@Override
-	public List<CategoryResponse> getActiveCategory() {
-		
-    	List<Category>categories = categoryRepo.findByIsActiveTrueAndDeletedFalse();
+	public Boolean saveCategory(CategoryDto categoryDto) {
 
-    	List<CategoryResponse> categoryList = categories.stream().map(cat->mapper.map(cat, CategoryResponse.class)).toList();
-    	
-		return categoryList;
+		Category category = mapper.map(categoryDto, Category.class);
+
+		if (ObjectUtils.isEmpty(category.getId())) {
+			category.setIsDeleted(false);
+			category.setCreatedBy(1);
+			category.setCreatedOn(new Date());
+		} else {
+			updateCategory(category);
+		}
+
+		Category saveCategory = categoryRepo.save(category);
+		return !ObjectUtils.isEmpty(saveCategory);
+	}
+
+	private void updateCategory(Category category) {
+		Optional<Category> findById = categoryRepo.findById(category.getId());
+		if (findById.isPresent()) {
+			Category existCategory = findById.get();
+			category.setCreatedBy(existCategory.getCreatedBy());
+			category.setCreatedOn(existCategory.getCreatedOn());
+			category.setIsDeleted(existCategory.getIsDeleted());
+
+			category.setUpdatedBy(1);
+			category.setUpdatedOn(new Date());
+		}
+	}
+
+	@Override
+	public List<CategoryDto> getAllCategory() {
+		List<Category> categories = categoryRepo.findAll();
+		return categories.stream().map(cat -> mapper.map(cat, CategoryDto.class)).toList();
+	}
+
+	@Override
+	public List<CategoryResponse> getActiveCategory() {  // fixed return type and variable names
+
+		List<Category> categories = categoryRepo.findByIsActiveTrueAndIsDeletedFalse();
+		return categories.stream().map(cat -> mapper.map(cat, CategoryResponse.class)).toList();
 	}
 
 	@Override
 	public CategoryDto getCategoryById(Integer id) {
-		
-		Optional<Category> findByCatgory=categoryRepo.findByIdAndIsDeletedFalse(id);
-		
-		if(findByCatgory.isPresent())
-		{
-			Category category = findByCatgory.get();
-			return mapper.map(category,CategoryDto.class);
+		Optional<Category> findByCategory = categoryRepo.findByIdAndIsDeletedFalse(id);
+		if (findByCategory.isPresent()) {
+			Category category = findByCategory.get();
+			return mapper.map(category, CategoryDto.class);
 		}
 		return null;
 	}
 
 	@Override
 	public Boolean deleteCategory(Integer id) {
-		Optional<Category> findByCatgory=categoryRepo.findById(id);
-
-		if(findByCatgory.isPresent())
-		{
-			Category category = findByCatgory.get();
+		Optional<Category> findByCategory = categoryRepo.findById(id);
+		if (findByCategory.isPresent()) {
+			Category category = findByCategory.get();
 			category.setIsDeleted(true);
 			categoryRepo.save(category);
 			return true;
 		}
-		
 		return false;
 	}
-	
-	
-	
-	
+
 }
